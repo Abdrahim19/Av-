0ee2b8f483431c7e60c3245c6c2631c022e12df4
hamzaAbdrahim <rsstrr7@gmail.com>
2023-12-24 16:32:34 +0100

    fix_responsiv

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Navbar from './components/Header/Navbar';
 import Hero from './pages/Home/Hero';
-import Heilighta_App from './pages/Home/Heilighta_App';
-import Requests from './pages/Home/dérniéres/Requests';
+// import Heilighta_App from './pages/Home/Heilighta_App';
+// import Requests from './pages/Home/dérniéres/Requests';
 
 
 
